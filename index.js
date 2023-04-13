4eb97d99d0b6eab2000ce7639e81f49c3bf645b9
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-04-13 15:41:46 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,58 +1,38 @@
-const dodger = document.getElementById('dodger');
-dodger.style.backgroundColor = "#ff69b4";
-dodger.style.width = "28px";
-dodger.style.left = "189px";
-const gameField = document.getElementById('game');
-gameField.style.width = '406px';
-const doggie = document.createElement('img');
-doggie.src = "./dog.jpg";
-doggie.style.height = "20px";
-dodger.appendChild(doggie);
-
-function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-    if (left > 0) {
-        dodger.style.left = `${left - 7}px`;
-    }
-}
-
-function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-    if (left < 378) {
-        dodger.style.left = `${left + 7}px`;
-    }
-}
-
-function moveDodgerUp() {
-    const bottomNumbers = dodger.style.bottom.replace("px", "");
-    const bottom = parseInt(bottomNumbers, 10);
-    if (bottom < 380) {
-        dodger.style.bottom = `${bottom + 10}px`;
-    }
-}
-
-function moveDodgerDown() {
-    const bottomNumbers = dodger.style.bottom.replace("px", "");
-    const bottom = parseInt(bottomNumbers, 10);
-    if (bottom > 0) {
-        dodger.style.bottom = `${bottom - 10}px`;
-    }
-}
-
-
-document.addEventListener("keydown", function (event) {
-    if (event.key === "ArrowLeft") {
+const dodger = document.querySelector('#dodger');
+dodger.style.backgroundColor = '#FF69B4';
+document.addEventListener('keydown', moveDodger);
+function moveDodger(e) {
+    if (e.key === 'ArrowLeft') {
         moveDodgerLeft();
-    }
-    else if (event.key === "ArrowRight") {
+    } else if (e.key === 'ArrowRight') {
         moveDodgerRight();
-    }
-    else if (event.key === "ArrowUp"){
+    } else if (e.key === 'ArrowUp') {
         moveDodgerUp();
-    }
-    else if (event.key === "ArrowDown"){
+    } else if (e.key === 'ArrowDown') {
         moveDodgerDown();
     }
-  });
+}
+function moveDodgerLeft() {
+    let number = parseInt(dodger.style.left.replace('px',''), 10);
+    if (number > 0) {
+        dodger.style.left = `${number - 10}px`;
+    }
+}
+function moveDodgerRight() {
+    let number = parseInt(dodger.style.left.replace('px',''), 10);
+    if (number < 360) {
+        dodger.style.left = `${number + 10}px`;
+    }
+}
+function moveDodgerUp() {
+    let number = parseInt(dodger.style.bottom.replace('px',''), 10);
+    if (number < 380) {
+        dodger.style.bottom = `${number + 10}px`;
+    }
+}
+function moveDodgerDown() {
+    let number = parseInt(dodger.style.bottom.replace('px',''), 10);
+    if (number > 0) {
+        dodger.style.bottom = `${number - 10}px`;
+    }
+}
